444344fd3d46deb4d754a2421feb03f3ce18273c
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2024-04-12 20:21:39 +0200

    🎉 Initial state

---
+++ autoload/MusicPlayer.gd
@@ -0,0 +1,54 @@
+extends Node
+
+@onready var players = get_children() as Array[AudioStreamPlayer];
+@onready var current_player = null;
+
+func _ready():
+	for player in players:
+		player.volume_db = -60;
+		var stream = player.stream;
+		stream.loop = true;
+	
+func play_music(music_name: String):
+	# prevent stopping music if it is playing already:
+	if current_player and current_player.name == music_name:
+		return;
+
+	for player in players:
+		player.play();
+	
+	fade_to(music_name);
+
+func fade_to(music_name):
+	if current_player and current_player.name == music_name:
+		return;
+	
+	var player = get_node(music_name);
+	
+	var time = 0.5;
+	
+	var tween = get_tree().create_tween();
+	
+	if current_player:
+		# fade out current player
+		tween.tween_property(current_player, "volume_db", -40, time);
+	
+	# fade in
+	tween.tween_property(player, "volume_db", -14, time);
+	
+	current_player = player;
+	
+#
+#func play_music(music_name:String):
+#	if current_music_name == music_name:
+#		return;
+#
+#	stop();
+#
+#	current_music_name = music_name;
+#	stream = music_streams.get(current_music_name);
+#
+#	play();
+#
+#func register_stream(stream_path:String, music_name:String):
+#	music_streams[music_name] = load(stream_path);
---
+++ screens/StartScreen/StartScreen.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+@onready var logo = $Logo;
+@onready var black_overlay = $BlackOverlay;
+@onready var start_button = $StartButton;
+@onready var credits_button = $CreditsButton;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# prepare intro
+	
+	# hide the buttons
+	start_button.hide();
+	credits_button.hide();
+	
+	# show a black screen
+	black_overlay.show();
+	
+	# make logo transparent
+	#logo.modulate = Color.TRANSPARENT;
+	
+	MusicPlayer.play_music("main");
+
+	await Game.wait(1);
+	
+	fade_in_logo();
+	fade_in_from_black();
+	
+	await Game.wait(2);
+	
+	start_button.show();
+	credits_button.show();
+
+func _process(_delta):
+	pass;
+	#var f = Game.anim_frame % 8;
+	#
+	#$AnimatedSprite2D.frame = f;
+	
+func fade_in_logo():
+	if (!is_instance_valid(logo)):
+		return;
+	var tween = create_tween().tween_property(logo, "modulate", Color.WHITE, 2);
+	await tween.finished;
+	
+func fade_in_from_black():
+	var tween = get_tree().create_tween().tween_property(black_overlay, "modulate", Color.TRANSPARENT, 2);
+	await tween.finished;
+
+func _on_start_button_pressed():
+	SceneManager.change_scene("res://scenes/MainScene/MainScene.tscn");
+
+
+func _on_credits_button_pressed():
+	SceneManager.change_scene("res://screens/CreditsScreen/CreditsScreen.tscn");
